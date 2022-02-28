f4c6ec44b62521c9d3bda0a28ddb453cbafd9fb4
ilya <ilja-rezident@mail.ru>
2022-02-28 22:37:05 +0300

    домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// функция которая возвращает случайное целое число в пределах диапазона включительно.
 function getRandomNumber (minNumber, maxNumber) {
   minNumber = Math.ceil(minNumber);
   maxNumber = Math.floor(maxNumber);
@@ -7,11 +6,76 @@ function getRandomNumber (minNumber, maxNumber) {
   }
   return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber; //тело функции взято с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 }
-getRandomNumber(0, 100);
-
-//функция определяющая максимальную длину строки в комментариях
-//Максимальная длина комментария 140 символов
 
 const getMaxComment = (commentLength, maxSumbol) => commentLength <= maxSumbol;
 
 getMaxComment (0, 140);
+
+function generateData() { //генерирует 25 обьектов
+  const result = [];
+  for (let i = 1; i <= 25; i++) {
+    result.push(generateItem(i));
+  }
+  return result;
+}
+
+function generateItem(i) {
+  const generateComents = ();
+  return {
+    id: i,
+    url: 'photos/[i].jpg',
+    description: 'просто очередное фото',
+    likes: getRandomNumber(15, 200),
+    comments: generateComents
+  };
+};
+
+const commentsCount = getRandomNumber(0, 10);
+
+const comments = getRandomComments(commentsCount) {
+  return {
+  id: commentsCount,
+  avatar: 'img/avatar-6.svg',
+  message: 'В целом всё неплохо. Но не всё.',
+  name: randomNameIndex,
+  }
+};
+
+const NAMES = ['Анна',
+  'Ольга',
+  'Вика',
+  'Артур',
+  'Максим',
+  'Лера',
+  'Сергей',
+  'Владимир',
+  'Артём',
+  'Франсуаза',
+  'Жюстин',
+  'Амели',
+  'Пол',
+  'Влад',
+  'Дарья',
+  'Маргарита',
+  'Илья',
+  'Артур',
+  'Роман',
+  'Роберт',
+  'Татьяна',
+  'Светлана',
+  'Валерия',
+  'Игорь',
+  'Степан'
+]; //массив с именами, по числу объектов
+
+const generateComents = () => {
+  const randomId = getRandomNumber(0, 200);
+  const randomAvatar = getRandomNumber(0, 6);
+  const randomNameIndex = getRandomNumber(0, NAMES.length - 1);
+  return {
+    id: randomId,
+    avatar: `img/avatar-${randomAvatar[i]}.svg`,
+    message: 'В целом всё неплохо. Но не всё.',
+    name: NAMES[randomNameIndex]
+  };
+};
